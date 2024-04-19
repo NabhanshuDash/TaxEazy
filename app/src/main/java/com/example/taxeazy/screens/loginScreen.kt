0d3b254f8d635b5b175a48d2888aa11eecec0fef
M. Farhaan baig <farhaan2k4@gmail.com>
2024-04-20 03:30:14 +0530

    Login Page UI done

--- app/src/main/java/com/example/taxeazy/screens/loginScreen.kt
+++ app/src/main/java/com/example/taxeazy/screens/loginScreen.kt
@@ -1,46 +1,44 @@
 package com.example.taxeazy.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.taxeazy.components.*
-import com.example.taxeazy.models.UserData
+import com.example.taxeazy.components.CTextField
+import com.example.taxeazy.components.GreyButtonComponent
+import com.example.taxeazy.components.TextComponent
 
 @Composable
 fun LoginScreen() {
-    var userData by remember { mutableStateOf("") }
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
 
     Surface(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
+        color = Color.White,
+        modifier = Modifier.fillMaxSize()
     ) {
         Column(
             modifier = Modifier
-                .fillMaxSize().padding(16.dp),
+                .fillMaxSize()
+                .padding(16.dp),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             TextComponent(
-                value = "Sign Up",
+                value = "Login",
                 fontStyletxt = FontStyle.Normal,
                 fweight = FontWeight.Bold,
                 size = 24,
@@ -48,24 +46,32 @@ fun LoginScreen() {
                 colortxt = Color.Black,
                 alignment = TextAlign.Center
             )
-            Spacer(modifier = Modifier.height(16.dp))
-            NormalTextFieldComponent(
-                LabelValue = "Username",
-                img = Icons.Filled.Person,
 
+            Spacer(modifier = Modifier.height(16.dp))
+
+            CTextField(
+                value = username,
+                onValueChange = { username = it },
+                hint = "Username",
+                icon = Icons.Filled.Person
             )
 
             Spacer(modifier = Modifier.height(16.dp))
-            PasswordTextFieldComponent(
-                labelValue = "Password",
-                img = Icons.Filled.Lock,
 
-            )
+            CTextField(
+                value = password,
+                onValueChange = { password = it },
+                hint = "Password",
+                icon = Icons.Filled.Lock,
 
-            Spacer(modifier = Modifier.height(16.dp))
-            ButtonComponent(
+                )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            GreyButtonComponent(
                 value = "Login",
-                colortxt = Color.White
+                colortxt = Color.White,
+                onClick = { /* Handle login click */ }
             )
         }
     }
