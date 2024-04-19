6a5c38165d6854d642f7d6f81bb80374e1854a47
M. Farhaan baig <farhaan2k4@gmail.com>
2024-04-19 23:25:06 +0530

    test

--- app/src/main/java/com/example/taxeazy/MainActivity.kt
+++ app/src/main/java/com/example/taxeazy/MainActivity.kt
@@ -20,12 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TaxEazyTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+
             }
         }
     }
