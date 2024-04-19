d1d6d4fd1adbf9def143938ec605e3051f218e05
Gantavya <gantavya2022@gmail.com>
2024-04-20 02:34:24 +0530

    SignUp page created login screen started

--- app/src/main/java/com/example/taxeazy/screens/loginScreen.kt
+++ app/src/main/java/com/example/taxeazy/screens/loginScreen.kt
@@ -1,5 +1,13 @@
 package com.example.taxeazy.screens
 
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+
+@Composable
 fun loginScreen(){
+
+    Surface {
+        val
+    }
 
 }
--- app/src/main/java/com/example/taxeazy/screens/signUpScreenCA.kt
+++ app/src/main/java/com/example/taxeazy/screens/signUpScreenCA.kt
@@ -2,10 +2,14 @@ package com.example.taxeazy.screens
 
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import com.example.taxeazy.models.CaData
 
 @Composable
 fun signUpScreenCA(){
     Surface {
+        var caData by remember { mutableStateOf(CaData ("","","",false,"",0.0,"",""))
 
     }
 }
