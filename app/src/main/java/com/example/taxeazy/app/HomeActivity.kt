1703974c2075b7e94f68ff47aac20626f5799028
M. Farhaan baig <farhaan2k4@gmail.com>
2024-04-21 06:07:57 +0530

    few enhancements

--- app/src/main/java/com/example/taxeazy/app/HomeActivity.kt
+++ app/src/main/java/com/example/taxeazy/app/HomeActivity.kt
@@ -4,58 +4,74 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.compose.ui.unit.dp
 import com.example.taxeazy.R
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : ComponentActivity() {
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Check if user is already logged in
+        if (auth.currentUser != null) {
+            // User is already logged in, redirect to appropriate screen
+            redirectToScreen()
+            return
+        }
+
         setContent {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceAround,
+                verticalArrangement = Arrangement.SpaceEvenly,
                 modifier = Modifier.fillMaxSize()
             ) {
-
-                Button(onClick = {
-                    val navigate = Intent(this@HomeActivity, SignUpCa::class.java)
-                    startActivity(navigate)
-                }) {
-
-                    Text(text="Register as CA")
-
+                Button(
+                    onClick = {
+                        val navigate = Intent(this@HomeActivity, SignUpCa::class.java)
+                        startActivity(navigate)
+                    },
+                    modifier = Modifier.height(72.dp)
+                ) {
+                    Text(text = "Register as CA")
                 }
 
-                Button(onClick = {
-                    val navigate = Intent(this@HomeActivity, SignUpUser::class.java)
-                    startActivity(navigate)
-                }) {
-
-                    Text(text="Register as Business")
-
+                Button(
+                    onClick = {
+                        val navigate = Intent(this@HomeActivity, SignUpUser::class.java)
+                        startActivity(navigate)
+                    },
+                    modifier = Modifier.height(72.dp)
+                ) {
+                    Text(text = "Register as Business")
                 }
 
-                Button(onClick = {
-                    val navigate = Intent(this@HomeActivity, LoginActivity::class.java)
-                    startActivity(navigate)
-                }) {
-
-                    Text(text="Login")
-
+                Button(
+                    onClick = {
+                        val navigate = Intent(this@HomeActivity, LoginActivity::class.java)
+                        startActivity(navigate)
+                    },
+                    modifier = Modifier.height(72.dp)
+                ) {
+                    Text(text = "Login")
                 }
-
-
             }
         }
+    }
+
+    private fun redirectToScreen() {
+
+        val navigate = Intent(this@HomeActivity, LandingPageUser::class.java)
+        startActivity(navigate)
+        finish() // Finish the current activity to prevent user from coming back to HomeActivity
     }
 }
--- app/src/main/java/com/example/taxeazy/app/LoginActivity.kt
+++ app/src/main/java/com/example/taxeazy/app/LoginActivity.kt
@@ -4,12 +4,9 @@ package com.example.taxeazy.app
 
 import android.content.Intent
 import android.os.Bundle
-import android.provider.ContactsContract.CommonDataKinds.Email
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Lock
@@ -22,19 +19,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.taxeazy.app.LandingPageUser
 import com.example.taxeazy.components.CTextField
 import com.example.taxeazy.components.GreyButtonComponent
 import com.example.taxeazy.components.TextComponent
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.launch
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.remember
+import com.google.firebase.firestore.FirebaseFirestore
 
 class LoginActivity : ComponentActivity() {
     private val auth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
@@ -76,8 +70,7 @@ class LoginActivity : ComponentActivity() {
                         value = email,
                         onValueChange = { email = it },
                         hint = "Email",
-                        icon = Icons.Filled.Person,
-                        leadingIcon = { Icon(Icons.Filled.Person, contentDescription = "Email") }
+                        icon = Icons.Filled.Person
                     )
 
                     Spacer(modifier = Modifier.height(16.dp))
@@ -106,9 +99,22 @@ class LoginActivity : ComponentActivity() {
                                             .addOnCompleteListener { task ->
                                                 if (task.isSuccessful) {
                                                     // Login successful
-                                                    // Navigate to the next screen or perform actions
-                                                    val navigate = Intent(this@LoginActivity, LandingPageUser::class.java)
-                                                    startActivity(navigate)
+                                                    val user = auth.currentUser
+                                                    if (user != null) {
+                                                        // Check if the user exists in the CA database
+                                                        val isCA = checkIfCA(user.uid) // Assuming checkIfCA function returns true if user is CA, false otherwise
+                                                        // Set the boolean variable based on whether the user is CA or not
+                                                        if (isCA) {
+                                                            // User is CA
+                                                            navigateToLandingPage(isCA = true)
+                                                        } else {
+                                                            // Regular user
+                                                            navigateToLandingPage(isCA = false)
+                                                        }
+                                                    } else {
+                                                        // User is null
+                                                        showError("User not found")
+                                                    }
                                                 } else {
                                                     // Login failed
                                                     error = task.exception?.message ?: "An unknown error occurred"
@@ -125,6 +131,7 @@ class LoginActivity : ComponentActivity() {
                         }
                     )
 
+
                     error?.let {
                         TextComponent(
                             value = it,
@@ -140,9 +147,39 @@ class LoginActivity : ComponentActivity() {
             }
         }
     }
-    private fun navigateToLandingPage() {
-        val navigate = Intent(this@LoginActivity, LandingPageUser::class.java)
-        startActivity(navigate)
+
+    private fun checkIfCA(uid: String): Boolean {
+        var isCA = false
+        val db = FirebaseFirestore.getInstance()
+
+        // Assuming you have a collection named "CAUsers" where CA information is stored
+        db.collection("ca").document(uid)
+            .get()
+            .addOnSuccessListener { document ->
+                if (document.exists()) {
+                    // User exists in the CA database
+                    isCA = true
+                }
+            }
+            .addOnFailureListener { exception ->
+                // Handle any errors
+                println("Error checking if user is a CA: $exception")
+            }
+
+        return isCA
+    }
+
+    private fun navigateToLandingPage(isCA: Boolean) {
+        // Pass the boolean value to the next screen using Intent
+        if(isCA) {
+            val navigate = Intent(this@LoginActivity, LandingPageCa::class.java)
+            startActivity(navigate)
+        }
+
+        else{
+            val navigate = Intent(this@LoginActivity, LandingPageUser::class.java)
+            startActivity(navigate)
+        }
     }
 
     private fun showError(message: String) {
@@ -150,4 +187,5 @@ class LoginActivity : ComponentActivity() {
         Toast.makeText(this@LoginActivity, message, Toast.LENGTH_SHORT).show()
     }
 }
+
 
